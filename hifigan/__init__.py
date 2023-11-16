f7375b0ac9f277a49e336f490f2bbcea478cc481
Rein Liu <50271800+Reinliu@users.noreply.github.com>
2023-11-16 20:40:56 +1100

    Update __init__.py

--- hifigan/__init__.py
+++ hifigan/__init__.py
@@ -5,3 +5,6 @@ class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+
+
+## The pretrained HIFIGAN vocoder could be accessed from: https://zenodo.org/records/7884686#.ZFBb6ezMKbh
